fedc6de44127e7fff460d029834b24a405b107e6
Elver581 <elveral100@gmail.com>
2026-07-27 11:27:08 -0500

    feat: agregar vista de edición de presupuesto

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -58,7 +58,9 @@ class BudgetController extends Controller
      */
     public function edit(Budget $budget)
     {
-        //
+        return view('budgets.edit',  [
+          'budget' => $budget
+     ]);
     }
 
     /**
--- app/View/Components/BudgetForm.php
+++ app/View/Components/BudgetForm.php
@@ -2,18 +2,18 @@
 
 namespace App\View\Components;
 
+use App\Http\Controllers\BudgetController;
+use App\Models\Budget;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class BudgetForm extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
+    public ?Budget $budget;
+        public function __construct(Budget $budget)
     {
-        //
+        $this->budget = $budget;
     }
 
     /**
