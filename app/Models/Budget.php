e18df8c8087ad764ebe45bff2ac1ba3a7595625d
Elver581 <elveral100@gmail.com>
2026-07-29 21:24:02 -0500

    test: validar presupuestos del usuario autenticado

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\BudgetType;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,7 +13,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Budget extends Model
 {
 
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
      protected $casts = [
         'type' => BudgetType::class
      ];
